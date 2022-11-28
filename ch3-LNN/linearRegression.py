b01d7febf8bbdf85699758e824a7940f2589393b
chaser-gua <chaser_gua@qq.com>
2022-11-28 20:42:23 +0800

    week13

---
+++ ch3-LNN/linearRegression.py
@@ -0,0 +1,95 @@
+# 从0实现一个线行神经网络，理解训练过程
+
+import random
+import torch
+from d2l import torch as d2l
+
+
+# 随机产生本次实验需要用的数据集
+def synthetic_data(w, b, num_examples):  # @save
+    """生成y=Xw+b+噪声"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
+
+
+# 打乱数据集中的样本，并且小批量方式获取数据
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    # print(num_examples)
+    indices = list(range(num_examples))
+    # print(indices)
+    # 这些样本是随机读取的，没有特定的顺序
+    random.shuffle(indices)
+    # print(indices)
+    for i in range(0, num_examples, batch_size):
+        batch_indices = torch.tensor(
+            indices[i: min(i + batch_size, num_examples)])
+        # print(batch_indices)
+        # 可以tensor中直接根据tensor取
+        yield features[batch_indices], labels[batch_indices]
+
+
+batch_size = 10
+
+for X, y in data_iter(batch_size, features, labels):
+    print(X, '\n', y)
+    break
+
+# 刚开始的时候初始化模型参数，这2个参数是我们最终要求优化的目标，训练的目标
+w = torch.normal(0, 0.01, size=(2,1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+
+# 定义模型
+def linreg(X, w, b):  #@save
+    """线性回归模型"""
+    return torch.matmul(X, w) + b
+
+
+# 定义损失函数
+def squared_loss(y_hat, y):  #@save
+    """均方损失"""
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+
+# 优化函数
+# params是我每次要迭代更新的参数的值
+def sgd(params, lr, batch_size):  #@save
+    """小批量随机梯度下降"""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size       # 更新
+            param.grad.zero_()
+
+
+# 最终的训练
+# 定义一系列超参数
+lr = 0.03    #学习率
+num_epochs = 3      #迭代次数
+
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):
+    for X, y in data_iter(batch_size, features, labels):
+        l = loss(net(X, w, b), y)  # X和y的小批量损失
+        # 因为l形状是(batch_size,1)，而不是一个标量。l中的所有元素被加到一起，
+        # 并以此计算关于[w,b]的梯度
+        l.sum().backward()
+        sgd([w, b], lr, batch_size)  # 使用参数的梯度更新参数
+    with torch.no_grad():
+        train_l = loss(net(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+
+print(f'w的估计误差: {true_w - w.reshape(true_w.shape)}')
+print(f'b的估计误差: {true_b - b}')
+
+
+
